2c2b108c04d6c40ad10a1fd9e9f64de10ab0404b
jeramyleon <jeramyleon@gmail.com>
2022-06-22 16:27:31 -0400

    progress

--- 1-python-primer/1-1-gpa-calculator.py
+++ 1-python-primer/1-1-gpa-calculator.py
@@ -1,6 +1,6 @@
-print('Welcome to the GPA calculator.')
-print('Please enter all your letter grades, one per line.')
-print('Enter a blank line to designate the end.')
+print("""Welcome to the GPA calculator.
+Please enter all your letter grades, one per line.
+Enter a blank line to designate the end.""")
 # map from letter grade to point value 
 points = {'A+':4.0, 'A':4.0, 'A-':3.67, 'B+':3.33, 'B':3.0,
 'B-':2.67, 'C+':2.33, 'C':2.0, 'C-':1.67, 'D+':1.33, 'D':1.0,
